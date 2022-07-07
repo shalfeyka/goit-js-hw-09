1d8d71e743f1ea722b8055179e95f9d324f0e5fd
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-08 02:20:25 +0300

    done with mistake task2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,1 +1,39 @@
 import Notiflix from 'notiflix';
+const references = {
+  inputStep: document.querySelector('[name="step"]'),
+  inputDelay: document.querySelector('[name="delay"]'),
+  btnSubmit: document.querySelector('button'),
+  inputAmount: document.querySelector('[name="amount"]'),
+  form: document.querySelector('form')
+}
+
+references.form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  let user = parseInt(references.inputDelay.value);
+  const userAmount = parseInt(references.inputAmount.value);
+  const userStep = parseInt(references.inputStep.value);
+
+  if(user >= 0 && userAmount >= 0 && userStep >= 0) {
+    for(let position = 1; position <= userAmount; position += 1) {
+      createPromise (position, user)
+        .then(value => {Notiflix.Notify.success(value);;})
+        .catch(error => {
+          Notiflix.Notify.warning(error);
+        });
+      user +=userStep;
+  }}
+});
+
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);          
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    })
+  })
+}
