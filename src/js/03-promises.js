576210e05450de1de75a5a613ced8c826d54b89d
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-07 22:27:34 +0300

    в процессе 3 задача

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,1 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-const form = document.querySelector('form');
-
-function createPromise(position, delay) {
-
-
-
-  
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
