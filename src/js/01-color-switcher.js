0894f89c2a4d7595f3415d61b9383105546932b3
Marina Checheneva <m.checheneva140201@gmail.com>
2022-07-07 21:04:19 +0300

    add references in 1 js task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,5 @@
-
+const references = {
+    buttonnStart: document.querySelector('button[data-start]'),
+    buttonStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
